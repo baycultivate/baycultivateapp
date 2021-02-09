f06375e08bf8538a5e9843d8b190f5c4744ee9dd
Paul Valderama <paulvalderama@Pauls-MacBook-Pro.local>
2021-02-08 19:43:56 -0800

    grey background full width

--- src/App.js
+++ src/App.js
@@ -37,19 +37,16 @@ function App () {
           <li style={liStyle}>Digital Marketing: SEO, Keyword Search, Social Media Campaigns Articles, Reviews, Ad Scheduling, Google Ads.</li>
         </ul>
       </div>
-
-      <div>
-        
+  
         <div style={{"backgroundColor":"silver"}}>
           <span style={{'fontFamily': 'Brush Script MT, cursive', 'fontSize':'30px'}}>Bay Cultivate Partners</span>
           <div style={{"display":"flex"}}>
-
-          <a href="http://c0d3.com" style={{"marginLeft":"75px", 'marginTop':'15px','color':"#5440d8", 'fontWeight': 'bold', 'fontFamily': 'PT Mono', 'fontSize':'130px', 'textDecoration':'none'}}>C0D3</a>
-          <a href="http://pacquiaofoundation.org" style={{"marginLeft":'100px', "marginTop": "25px"}}><img src={'https://baycultivate.s3-us-west-1.amazonaws.com/MANNY-PACQUIAO-FOUNDATION.svg'}/></a>
-          <a href="https://3rdstreetboxing.com/" style={{"marginTop":"-20px","marginLeft":'150px'}}><img src={'https://baycultivate.s3-us-west-1.amazonaws.com/3rd-Street2colorlogov3.png'} style={{'height':'175px'}}/></a>
+            <a href="http://c0d3.com" style={{"marginLeft":"75px", 'marginTop':'15px','color':"#5440d8", 'fontWeight': 'bold', 'fontFamily': 'PT Mono', 'fontSize':'130px', 'textDecoration':'none'}}>C0D3</a>
+            <a href="http://pacquiaofoundation.org" style={{"marginLeft":'100px', "marginTop": "25px"}}><img src={'https://baycultivate.s3-us-west-1.amazonaws.com/MANNY-PACQUIAO-FOUNDATION.svg'}/></a>
+            <a href="https://3rdstreetboxing.com/" style={{"marginTop":"-20px","marginLeft":'150px'}}><img src={'https://baycultivate.s3-us-west-1.amazonaws.com/3rd-Street2colorlogov3.png'} style={{'height':'175px'}}/></a>
           </div>
         </div>
-      </div>
+
       </div>
     )
 } 
