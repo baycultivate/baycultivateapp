809bc1a5a11d818a4fccc58eb473aa824134fa6f
Paul Valderama <paulvalderama@Pauls-MacBook-Pro.local>
2021-02-08 19:23:38 -0800

    partners added

--- src/App.js
+++ src/App.js
@@ -20,21 +20,36 @@ function App () {
   const responsive = {
     showTopNavMenu: width > 1023
   }
+
+  const liStyle={"marginTop":"100px", "fontWeight":"bold", "fontSize":"50px"}
     return (
       <div>
-        <img src={'https://baycultivate.s3-us-west-1.amazonaws.com/logo+last.PNG'} alt="Logo" style={{"display": "block", "margin-left": "auto", "margin-top": "25px", "margin-right": "auto", "height": "400px", "width": "400px"}}/>
-        
+        <img src={'https://baycultivate.s3-us-west-1.amazonaws.com/logo+last.PNG'} alt="Logo" style={{"display": "block", "marginLeft": "auto", "marginTop": "25px", "marginRight": "auto", "height": "400px", "width": "400px"}}/>                
+
+      <div>
         <ul> 
-          <li>Web Development: professional template style, quick turnaround, option to upgrade by feature</li>          
+          <li style={liStyle}>Web Development: professional template style, quick turnaround, option to upgrade by feature.</li>          
         </ul>
         <ul>
-          <li>Software Engineering: Individually distinct web design that is fully customizable, better long term investment</li>
+          <li style={liStyle}>Software Engineering: Individually customized web design that is universally unique, better long term investment.</li>
         </ul>
         <ul>
-          <li>Digital Marketing: SEO, Keyword Search, Social Media Campaigns Articles, Reviews, Ad Scheduling, Google Ads</li>
+          <li style={liStyle}>Digital Marketing: SEO, Keyword Search, Social Media Campaigns Articles, Reviews, Ad Scheduling, Google Ads.</li>
         </ul>
-      
-      <button>Schedule Phonecall</button>
+      </div>
+
+      <div>
+        
+        <div style={{"backgroundColor":"silver"}}>
+          <span style={{'fontFamily': 'Brush Script MT, cursive', 'fontSize':'30px'}}>Bay Cultivate Partners</span>
+          <div style={{"display":"flex"}}>
+
+          <a href="http://c0d3.com" style={{"marginLeft":"75px", 'marginTop':'15px','color':"#5440d8", 'fontWeight': 'bold', 'fontFamily': 'PT Mono', 'fontSize':'130px', 'textDecoration':'none'}}>C0D3</a>
+          <a href="http://pacquiaofoundation.org" style={{"marginLeft":'100px', "marginTop": "25px"}}><img src={'https://baycultivate.s3-us-west-1.amazonaws.com/MANNY-PACQUIAO-FOUNDATION.svg'}/></a>
+          <a href="https://3rdstreetboxing.com/" style={{"marginTop":"-20px","marginLeft":'150px'}}><img src={'https://baycultivate.s3-us-west-1.amazonaws.com/3rd-Street2colorlogov3.png'} style={{'height':'175px'}}/></a>
+          </div>
+        </div>
+      </div>
       </div>
     )
 } 
