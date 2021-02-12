5663cec8de93bf6f06f27eb36ac81fbcc490ddfc
Paul Valderama <paulvalderama@Pauls-MacBook-Pro.local>
2021-02-12 09:52:45 -0800

    flexbox responsive

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component, useState, useEffect } from 'react';
+import './App.css'
 
 // import Introduction from './components/introduction'
 // import logo from './logo last.PNG'
@@ -20,33 +21,35 @@ function App () {
   const responsive = {
     showTopNavMenu: width > 1023
   }
-
-  const liStyle={"marginTop":"100px", "fontWeight":"bold", "fontSize":"50px"}
     return (
       <div>
-        <img src={'https://baycultivate.s3-us-west-1.amazonaws.com/logo+last.PNG'} alt="Logo" style={{"display": "block", "marginLeft": "auto", "marginTop": "25px", "marginRight": "auto", "height": "400px", "width": "400px"}}/>                
-
-      <div>
-        <ul> 
-          <li style={liStyle}>Web Development: professional template style, quick turnaround, option to upgrade by feature.</li>          
-        </ul>
-        <ul>
-          <li style={liStyle}>Software Engineering: Individually customized web design that is universally unique, better long term investment.</li>
-        </ul>
-        <ul>
-          <li style={liStyle}>Digital Marketing: SEO, Keyword Search, Social Media Campaigns Articles, Reviews, Ad Scheduling, Google Ads.</li>
-        </ul>
-      </div>
-  
-        <div style={{"backgroundColor":"silver"}}>
-          <span style={{'fontFamily': 'Brush Script MT, cursive', 'fontSize':'30px'}}>Bay Cultivate Partners</span>
-          <div style={{"display":"flex"}}>
-            <a href="http://c0d3.com" style={{"marginLeft":"75px", 'marginTop':'15px','color':"#5440d8", 'fontWeight': 'bold', 'fontFamily': 'PT Mono', 'fontSize':'130px', 'textDecoration':'none'}}>C0D3</a>
-            <a href="http://pacquiaofoundation.org" style={{"marginLeft":'100px', "marginTop": "25px"}}><img src={'https://baycultivate.s3-us-west-1.amazonaws.com/MANNY-PACQUIAO-FOUNDATION.svg'}/></a>
-            <a href="https://3rdstreetboxing.com/" style={{"marginTop":"-20px","marginLeft":'150px'}}><img src={'https://baycultivate.s3-us-west-1.amazonaws.com/3rd-Street2colorlogov3.png'} style={{'height':'175px'}}/></a>
-          </div>
+        <div className="container-banner">          
+            <img className="logo-1" src={'https://baycultivate.s3-us-west-1.amazonaws.com/logo+last.PNG'} alt="Logo" />                          
         </div>
 
+        <div className="container-services">
+          <ul> 
+            <li >Web Development: professional template style, quick turnaround, option to upgrade by feature.</li>          
+          </ul>
+          <ul>
+            <li >Software Engineering: Individually customized web design that is universally unique, better long term investment.</li>
+          </ul>
+          <ul>
+            <li >Digital Marketing: SEO, Keyword Search, Social Media Campaigns Articles, Reviews, Ad Scheduling, Google Ads.</li>
+          </ul>
+        </div>
+      
+        <div className="container-footer">
+
+          <div className="container-partners" >
+            <span className="partners-title" >Bay Cultivate Partners</span>
+            <div className="partners">
+              <a className="partners1" href="http://c0d3.com" >C0D3</a>
+              <a href="http://pacquiaofoundation.org" ><img className='partners2'src={'https://baycultivate.s3-us-west-1.amazonaws.com/MANNY-PACQUIAO-FOUNDATION.svg'}/></a>
+              <a href="https://3rdstreetboxing.com/" ><img className="partners3" src={'https://baycultivate.s3-us-west-1.amazonaws.com/3rd-Street2colorlogov3.png'} /></a>            
+            </div>
+          </div>
+        </div>
       </div>
     )
 } 
