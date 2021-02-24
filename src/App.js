04a6da1f14ace0195231c8a3221186bc44e134e9
Paul Valderama <paulvalderama@Pauls-MacBook-Pro.local>
2021-02-24 14:06:24 -0800

    spacing

--- src/App.js
+++ src/App.js
@@ -65,15 +65,16 @@ function App () {
                Bay Cultivate, <a href='https://pacquiaofoundation.org'>Manny Pacquiao Foundation</a>, and <a href='https://garagescript.org'>Garage Script</a> have collaborated to bring Tech Ed Programs to Philippines! Check out <a href='https://c0d3.com'> C0D3,</a> a free Software Engineer Curriclum created by Garage Script to give access to anyone in the world who wishes to learn.
              </li>
             
-            {/* </ul>
+            </ul>
              
-            <ul> */}
+            <ul>
               {/* <li >Software Engineering: Individually customized web design that is unique to any site online. Email to schedule engineering consult.</li> */}
             
               {/* <li >In collaboration with the <a href='https://pacquiaofoundation.org'>Manny Pacquiao Foundation</a>, we want to teach the C0D3 curriculum in the Philippines! As new programs are conceived, new funding must be allocated as well.</li> */}
 
               <li>Please reach out to the email above if you have any interest in getting involved by teaching, volunteering, financing, or donating.</li>
-              
+              </ul>
+              <ul>
               <li>As we navigate through COVID please do your part in social distancing and wearing your mask, it could save lives! Try to uphold community, check in with your loved ones and friends, and always have hope for tomorrow!</li>
             {/* </ul>
             <ul> */}
