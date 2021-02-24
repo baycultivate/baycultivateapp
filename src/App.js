556feb3ab717af9aca356862547ca7966de75bde
Paul Valderama <paulvalderama@Pauls-MacBook-Pro.local>
2021-02-24 13:37:00 -0800

    covid note

--- src/App.js
+++ src/App.js
@@ -57,15 +57,32 @@ function App () {
             {/* <div style={{'fontWeight': 'bold', 'marginTop': '10px',  'marginLeft':'10px', 'marginBottom': '20px'}}><GiSmartphone /> Phone: </div> */}
             <ul> 
               {/* <li >Web Development: professional template style, quick turnaround, option to upgrade by feature. $50 month and up, email to go over web template options.</li>           */}
-              <li> <a href='https://garagescript.org'>Garage Script</a> is a 501c3 non-profit that teaches Software Engineering. Their curriclum <a href='https://c0d3.com'>C0D3</a> is proven as past graduates now work at Google as Software Engineers. Classes were being taught in Silicon Valley pre-Covid and are waiting to begin again. Donations made to the organization provide salaries for the staff to teach.</li>          
-            </ul>
-            <ul>
+              
+              {/* <li> <a href='https://garagescript.org'>Garage Script</a> is a 501c3 non-profit that teaches Software Engineering. Their curriclum <a href='https://c0d3.com'>C0D3</a> is proven as past graduates now work at Google as Software Engineers. Classes were being taught in Silicon Valley pre-Covid and are waiting to begin again. Donations made to the organization provide salaries for the staff to teach.</li>          
+             */}
+
+             <li>
+               Bay Cultivate, <a href='https://pacquiaofoundation.org'>Manny Pacquiao Foundation</a>, and <a href='https://garagescript.org'>Garage Script</a> have collaborated to bring Tech Ed Programs to Philippines! Check out <a href='https://c0d3.com'> C0D3,</a> a free Software Engineer Curriclum created by Garage Script to give access to anyone in the world who wishes to learn.
+             </li>
+            
+            {/* </ul>
+             
+            <ul> */}
               {/* <li >Software Engineering: Individually customized web design that is unique to any site online. Email to schedule engineering consult.</li> */}
-              <li >In collaboration with the <a href='https://pacquiaofoundation.org'>Manny Pacquiao Foundation</a>, we want to teach the C0D3 curriculum in the Philippines! As new programs are conceived, new funding must be allocated as well.</li>
-            </ul>
-            <ul>
+            
+              {/* <li >In collaboration with the <a href='https://pacquiaofoundation.org'>Manny Pacquiao Foundation</a>, we want to teach the C0D3 curriculum in the Philippines! As new programs are conceived, new funding must be allocated as well.</li> */}
+
+              <li>Please reach out to the email above if you have any interest in getting involved by teaching, volunteering, financing, or donating.</li>
+              
+              <li>As we navigate through COVID please do your part in social distancing and wearing your mask, it could save lives! Try to uphold community, check in with your loved ones and friends, and always have hope for tomorrow!</li>
+            {/* </ul>
+            <ul> */}
               {/* <li >Digital Marketing: SEO, Keyword Search, Social Media Campaigns Articles, Reviews, Ad Scheduling, Google Ads. Email to schedule services call.</li> */}
-              <li >Reach out to the email above for any questions or interest in providing financial contributions, Thank You.</li>
+            
+              {/* <li >Reach out to the email above for any questions or interest in providing financial contributions, Thank You.</li> */}
+            
+
+
             </ul>
             
           </div>
